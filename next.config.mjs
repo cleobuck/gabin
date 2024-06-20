ad062dbe65d3b6fcc6e1dd578b5d0cb58f5d7cf4
cleobuck <cleo.buck@entaingroup.com>
2024-06-20 14:46:06 +0200

    add initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require("path");
+const withLess = require("next-with-less");
 
-export default nextConfig;
+module.exports = withLess({
+  // reactStrictMode: true,
+  lessLoaderOptions: {
+    lessOptions: {
+      javascriptEnabled: true,
+    },
+  },
+  webpack(config) {
+    // Define path alias for styles directory
+    config.resolve.alias["@styles"] = path.join(__dirname, "src/assets/styles");
+    config.resolve.alias["@fonts"] = path.join(__dirname, "src/assets/fonts");
+
+    // Add webpack rule for SVG files
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+});
