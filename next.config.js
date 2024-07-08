5b18295dfb7218f6794ffaec5ebe22fdc0b4a1ee
cleobuck <cleo.buck@entaingroup.com>
2024-07-08 20:51:29 +0200

    add stage url

--- next.config.js
+++ next.config.js
@@ -19,6 +19,6 @@ module.exports = {
     BASE_URL:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
-        : "https://your-production-url.com",
+        : "https://main--animated-liger-392e0d.netlify.app/",
   },
 };
